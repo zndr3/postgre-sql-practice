bdfaa9b9e49d673c856da4edf3c3fe4db7921755
zndr3 <zandersison18@gmail.com>
2026-04-03 22:38:25 +0800

    UPDATE: used CASE, subquery, self INNER and LEFT JOIN

--- sports_club/main.py
+++ sports_club/main.py
@@ -8,3 +8,15 @@ print(get_facilities_and_membercost())
 
 print("\nFacilities that Charge:")
 print(get_facilities_that_charges())
+
+print("\nCheap and Expensive Facilities:")
+print(get_cheap_and_expensive_facilities())
+
+print("\nLatest Joined Date:")
+print(get_latest_joined_date())
+
+print("\nPeople That Recommended:")
+print(get_people_that_recommended())
+
+print("\nMember and Recommended By:")
+print(get_member_and_recommended_by())
--- sports_club/queries.py
+++ sports_club/queries.py
@@ -21,3 +21,38 @@ def get_facilities_that_charges():
         FROM cd.facilities
         WHERE membercost > 0
         """, engine)
+
+def get_cheap_and_expensive_facilities():
+    return pd.read_sql("""
+        SELECT name, 
+        CASE WHEN monthlymaintenance > 100 THEN 'expensive'
+        ELSE 'cheap'
+        END AS cost
+        FROM cd.facilities;
+        """, engine)
+
+def get_latest_joined_date():
+    return pd.read_sql("""
+        SELECT firstname, surname, joindate
+        FROM cd.members
+        WHERE joindate = (SELECT MAX(joindate) FROM cd.members)
+        """, engine)
+
+def get_people_that_recommended():
+    return pd.read_sql("""
+        SELECT DISTINCT(m2.firstname), m2.surname
+        FROM cd.members m1
+        INNER JOIN cd.members m2
+        ON m1.recommendedby = m2.memid
+        ORDER BY m2.surname;
+        """, engine)
+
+def get_member_and_recommended_by():
+    return pd.read_sql("""
+        SELECT member.firstname AS memfname, member.surname AS memsname, 
+	    reco.firstname AS recfname, reco.surname AS recsname
+        FROM cd.members member
+        LEFT JOIN cd.members reco
+        ON member.recommendedby = reco.memid
+        ORDER BY member.surname, member.firstname
+        """, engine)
